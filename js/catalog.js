aea3d6e0db04db8c317710010dda2f13dd0326cd
kovalyova-alena <kovalyova_alena@inbox.ru>
2017-05-04 23:55:37 +0300

    start to create filter functionality

--- js/catalog.js
+++
@@ -1,7 +0,0 @@
-function OfferBanner (offer) {
-    if (!offer) return;
-
-    this.offer = offer;
-    this.itemImg = document.querySelectorAll('.arrivalItem')[0];
-    this.itemImg.style.cssText= 'margin-bottom:' + this.offer.clientHeight + 'px';
-}
--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ App.prototype.init = function () {
     new Search();
     new Menu();
     new OfferBanner(document.querySelector('.extraOff'));
+    new Filter(document.querySelector('.formFilter'));
    // new ChangeMarkup();
 
 };
@@ -20,6 +21,8 @@ App.prototype.init = function () {
 Search.prototype = Object.create(App.prototype);
 ChangeMarkup.prototype = Object.create(App.prototype);
 Menu.prototype = Object.create(App.prototype);
+OfferBanner.prototype = Object.create(App.prototype);
+Filter.prototype = Object.create(App.prototype);
 
 window.addEventListener('resize', function(event){
     new OfferBanner(document.querySelector('.extraOff'));
@@ -40,6 +43,35 @@ function ChangeMarkup() {
 
 }
 
+function Filter (filter) {
+    if (!filter) return;
+
+    this.filterForm = filter;
+    this.tabletLabel = document.querySelector('.filterTablet');
+    this.desktopSelects = document.querySelector('.desktopSelects');
+    this.selectItems = document.querySelectorAll('.selectItem');
+    this.options = this.filterForm.querySelectorAll('option');
+    this.tabletLabel.addEventListener('click', this.openFilter.bind(this));
+
+    this.ww = window.innerWidth;
+    if (this.ww > 1024) {
+        this.desktopSelects.addEventListener('click', this.openSelect.bind(this));
+        for (var i = 0; i < this.options.length; i++) {
+            this.options[i].addEventListener('click', this.closeSelect.bind(this, this.options[i]));
+        }
+    }
+
+}
+
+function OfferBanner (offer) {
+    if (!offer) return;
+
+    this.offer = offer;
+    this.itemImg = document.querySelectorAll('.arrivalItem')[0];
+    this.itemImg.style.cssText= 'margin-bottom:' + this.offer.clientHeight + 'px;';
+}
+
+
 Search.prototype.openSearch = function () {
     document.querySelector('.search').classList.toggle('openSearch');
     document.querySelector('.searchForm').classList.toggle('display');
@@ -52,6 +84,34 @@ Menu.prototype.openMenu = function (e) {
     this.nav.classList.toggle('display');
 
 };
+
+Filter.prototype.openFilter = function () {
+    this.desktopSelects.classList.toggle('mobileSelects');
+};
+
+Filter.prototype.openSelect = function (e) {
+    var target = e && e.target || e.srcElement;
+
+    if (target.parentNode.getAttribute('class') != 'selectLabel') return;
+
+    target = target.parentNode.parentNode;
+    for (var i = 0; i < this.selectItems.length; i++) {
+        if (this.selectItems[i] != target) {
+            this.selectItems[i].querySelector('select').classList.remove('display');
+        }
+    }
+    target.querySelector('select').classList.toggle('display');
+};
+
+Filter.prototype.closeSelect = function (option) {
+    option.closest('select').classList.remove('display');
+    var nameFilter = document.querySelector('.nameFilter');
+    var valueFilter = document.querySelector('.valueFilter');
+    valueFilter.innerHTML = option.value;
+
+    
+};
+
 
 
 
